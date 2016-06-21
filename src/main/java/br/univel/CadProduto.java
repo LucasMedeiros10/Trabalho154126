10d297d904ca7faeb74c197bceb3e8011793a6b5
LucasMedeiros10 <lucas_medeiros09@hotmail.com>
2016-06-21 20:02:23 -0300

    Alterações cadastros gráficos

--- src/main/java/br/univel/CadProduto.java
+++ src/main/java/br/univel/CadProduto.java
@@ -19,8 +19,8 @@ public class CadProduto extends JFrame{
 	public JButton btnSalvar;
 	public JButton btnCancelar;
 	public JLabel lblTitulo;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtID;
+	private JTextField txtDescricao;
 	
 	public CadProduto(){
 		setTitle("Cadastro de Produto");
@@ -39,17 +39,17 @@ public class CadProduto extends JFrame{
 		lblTitulo.setVerticalAlignment(SwingConstants.TOP);
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 18));
 		
-		textField = new JTextField();
-		textField.setColumns(10);
+		txtID = new JTextField();
+		txtID.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("ID");
 		
 		JLabel lblNewLabel_1 = new JLabel("Descri\u00E7\u00E3o");
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
+		txtDescricao = new JTextField();
+		txtDescricao.setColumns(10);
 		
-		JFormattedTextField formattedTextField = new JFormattedTextField();
+		JFormattedTextField txtPreco = new JFormattedTextField();
 		
 		JLabel lblPreo = new JLabel("Pre\u00E7o");
 		groupLayout = new GroupLayout(getContentPane());
@@ -67,13 +67,13 @@ public class CadProduto extends JFrame{
 									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
 										.addComponent(lblNewLabel)
 										.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-										.addComponent(textField, 0, 0, Short.MAX_VALUE))
+										.addComponent(txtID, 0, 0, Short.MAX_VALUE))
 									.addPreferredGap(ComponentPlacement.RELATED, 266, Short.MAX_VALUE)
 									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
 										.addComponent(btnCancelar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 										.addComponent(btnSalvar, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)))
-								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
-								.addComponent(formattedTextField, GroupLayout.PREFERRED_SIZE, 79, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtDescricao, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtPreco, GroupLayout.PREFERRED_SIZE, 79, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblPreo))))
 					.addContainerGap())
 		);
@@ -89,15 +89,15 @@ public class CadProduto extends JFrame{
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(btnCancelar)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(lblNewLabel_1)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(txtDescricao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(lblPreo)
 					.addGap(3)
-					.addComponent(formattedTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(txtPreco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(53, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
